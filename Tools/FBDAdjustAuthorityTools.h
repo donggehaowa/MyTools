c8183a94dd4ae4030bb55e3bc6806d54a069f92c
feng <feng@fengdeMacBook-Pro.local>
2016-12-08 16:57:20 +0800

    update for 去掉 header.h file

--- Tools/FBDAdjustAuthorityTools.h
+++ Tools/FBDAdjustAuthorityTools.h
@@ -5,6 +5,17 @@
 //  Created by feng on 16/4/29.
 //  Copyright © 2016年 Bioon. All rights reserved.
 //
+//对话框
+#define SHOW_ALERT(msg) UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"温馨提醒" message:msg delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil];\
+[alert show];
+
+
+#define SHOW_ALERTdiss(msg) UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"温馨提示" message:msg delegate:nil cancelButtonTitle:nil otherButtonTitles:nil, nil];\
+[alert show];\
+dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{\
+[alert dismissWithClickedButtonIndex:0 animated:YES];\
+});
+
 
 #import <Foundation/Foundation.h>
 
